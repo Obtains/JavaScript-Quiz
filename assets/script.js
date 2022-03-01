fe225837280a1aca47928e6624d860fe382e7314
Failsafe <hawkhilt@gmail.com>
2022-03-01 11:33:46 -0500

    add objects for ids in js

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+const bestscoreDiv = document.querySelector("#bestscore");
+const contentEl = document.querySelector("#content");
+const quizTimerEl = document.querySelector("#quizTimer");
+const timerInfoEl = document.querySelector("#timer");
+
