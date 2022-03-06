9e9f2a7b3778095cbf3cd65eda6cb5cb26c9a75a
Failsafe <hawkhilt@gmail.com>
2022-03-06 02:43:17 -0500

    add another JS to hold the scores and view on new html page

--- assets/bestscores.js
+++ assets/bestscores.js
@@ -1,1 +1,23 @@
+const bestScore = document.querySelector("#bestScore");
+const refresh = document.querySelector("#refresh");
+const restartQuiz = document.querySelector("#restartQuiz");
 
+refresh.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+var leaderScores = localStorage.getItem("leaderScores");
+leaderScores = JSON.parse(leaderScores);
+
+if (leaderScores !== null) {
+    for ( var i = 0; i < leaderScores.length; i++) {
+        var liData = document.createElement("li");
+        liData.textContent = leaderScores[i].initials + " " + leaderScores[i].score;
+        bestScore.appendChild(liData);
+    }
+}
+
+restartQuiz.addEventListener("click", function () {
+    window.location.replace("index.html");
+});
